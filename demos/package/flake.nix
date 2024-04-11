5cd51ecf8b4ff8d9e7ba18f076eefdfb2dd3ae73
Minijackson <minijackson@riseup.net>
2024-04-11 13:19:56 +0200

    feat(package): init demo

---
+++ demos/package/flake.nix
@@ -0,0 +1,34 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in {
+    packages.x86_64-linux.default = pkgs.stdenv.mkDerivation {
+      pname = "production-grade-hello-hepix";
+      version = "1.0.0";
+
+      src = ./.;
+
+      nativeBuildInputs = [
+        pkgs.meson
+        pkgs.ninja
+        pkgs.pkg-config
+      ];
+
+      buildInputs = [
+        pkgs.boost
+      ];
+
+      env = {
+        BOOST_INCLUDEDIR = "${pkgs.lib.getDev pkgs.boost}/include";
+        BOOST_LIBRARYDIR = "${pkgs.lib.getLib pkgs.boost}/lib";
+      };
+
+      doCheck = true;
+    };
+  };
+}
