b3fb85a231d619e6f2afe54ae7ded01707ff8680
Minijackson <minijackson@riseup.net>
2024-04-17 08:57:58 +0200

    feat(nixos-offline): init demo

---
+++ demos/nixos-offline/flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, disko, ... }:
+    {
+      nixosConfigurations.nixos-hepix-demo-offline = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          disko.nixosModules.disko
+          ./configuration.nix
+        ];
+      };
+    };
+}
